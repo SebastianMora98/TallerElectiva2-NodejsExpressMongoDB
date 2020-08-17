a04ce10a8676cb3be09cf5e9400f7ce39cc68b64
SebastianMora98 <jhonsebastianmora@gmail.com>
2020-08-17 18:23:24 -0500

    nuevo archivo para rutas, se agrego body-parser

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Import Routes
+const citasRoute = require('./routes/citas');
+
+// Middlewares
+app.use('/citas', citasRoute);
 
 // Routes
 app.get('/', (req, res) => {
